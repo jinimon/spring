eb4e0fd5b8aad3a61c1727204589b4babd47421f
jinimon <realpcy04@gmail.com>
2021-01-12 09:41:21 +0900

    0112_엑셀다운

--- spring/src/main/java/co/company/spring/config/MvcConfiguration.java
+++ spring/src/main/java/co/company/spring/config/MvcConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import co.company.spring.common.AuthCheckInterceptor;
@@ -106,5 +107,12 @@ public class MvcConfiguration implements WebMvcConfigurer {
 		// setUploadTempDir : 이거 생략하면 각각 가지고 있는 임시폴더에 저장.
 		multi.setMaxUploadSize(1024*10000);	// 10MB
 		return multi;
+	}
+	
+	@Bean
+	BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver bean = new BeanNameViewResolver();
+		bean.setOrder(1);
+		return bean;
 	}
 }
